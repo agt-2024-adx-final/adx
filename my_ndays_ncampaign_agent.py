21f544406e8585e13c6c209658a88da5212860e7
Mason Stang <masonastang@gmail.comgit config --global user.name Mason-Stang>
2024-04-25 08:48:17 -0400

    first comp 1 submission

--- my_ndays_ncampaign_agent.py
+++ my_ndays_ncampaign_agent.py
@@ -23,6 +23,14 @@ class MyNDaysNCampaignsAgent(NDaysNCampaignsAgent):
         quality_score_alpha = 1 - (self.get_current_day() * 0.01) # more days left --> bid more
         quality_score = self.get_quality_score()
 
+        # quality_score_alpha_2 seems to not actually be helping!
+        if quality_score < 0.7:
+            quality_score_alpha_2 = 1.2
+        elif quality_score < 0.5:
+            quality_score_alpha_2 = 2
+        else:
+            quality_score_alpha_2 = 1
+
         for campaign in self.get_active_campaigns():
             bids = set()
             days_left = campaign.end_day - self.get_current_day()
@@ -30,7 +38,10 @@ class MyNDaysNCampaignsAgent(NDaysNCampaignsAgent):
             budget_left = campaign.budget - self.get_cumulative_cost(campaign)
 
             campaign_segment = campaign.target_segment
-            initial_bid = budget_left / reach_left
+            if reach_left == 0:
+                initial_bid = 0
+            else:
+                initial_bid = budget_left / reach_left
 
             days_left_alpha = 1 - (.05 * days_left) # more days left --> bid less
             percent_reach_left = (campaign.reach - self.get_cumulative_reach(campaign)) / campaign.reach
@@ -44,11 +55,11 @@ class MyNDaysNCampaignsAgent(NDaysNCampaignsAgent):
                 if len(segment) == 2:   segment_scalar = .9
                 else:                   segment_scalar = 1
 
-                bid_per_item = initial_bid * segment_scalar * quality_score_alpha * days_left_alpha * reach_alpha #*10
+                bid_per_item = initial_bid * segment_scalar * quality_score_alpha * days_left_alpha * reach_alpha #* quality_score_alpha_2
 
                 bid_limit = budget_left
                 bid = Bid(self, segment, bid_per_item, bid_limit)
-                print("bid per item:", bid_per_item)
+                #print("bid per item:", bid_per_item)
                 bids.add(bid)
 
             campaign_limit = budget_left
@@ -61,12 +72,12 @@ class MyNDaysNCampaignsAgent(NDaysNCampaignsAgent):
         # reverse auction - lower bids win
         bids = {}
         for auction_campaign in campaigns_for_auction:
-            initial_bid = auction_campaign.reach * 0.1
+            initial_bid = auction_campaign.reach #* 0.8
 
             campaign_length = auction_campaign.end_day - auction_campaign.start_day # 0, 1, or 2
             campaign_alpha = 1 - .1 * campaign_length
 
-            if len(auction_campaign.target_segment) == 2:   specificity_alpha = 0.8
+            if len(auction_campaign.target_segment) == 2:   specificity_alpha = 0.9
             else:                                           specificity_alpha = 1
 
             issubset_scalar = 1
